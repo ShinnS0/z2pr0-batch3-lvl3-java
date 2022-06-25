ddc3491eb51df9c4209ab55e75bac0af74cbfb73
ShinnS0 <zwekaung151@gmail.com>
2022-06-25 12:19:36 +0630

    Day1

---
+++ 01-base/src/introduction/intro.java
@@ -0,0 +1,8 @@
+package introduction;
+
+public class intro {
+	public static void main(String[] args) {
+		String ggwp = "zwe kaung kyaw.";
+		System.out.println("My name is " + ggwp);
+	}
+}
---
+++ 01-base/src/personal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class personal {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		Scanner data = new Scanner(System.in);
+		int days = data.nextInt();
+		int hours = days*24;
+		int minutes = hours*60;
+		int seconds = minutes*60;
+		System.out.println(days + " days" + " = " + seconds + " seconds.");
+	}
+}
